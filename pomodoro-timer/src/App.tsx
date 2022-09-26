b0afafeb1fa4294e0330b5950124b24970cba583
Kuba Pawlak <kubapawlak1051@wp.pl>
2022-09-26 22:40:39 +0200

    Initial commit, created react app

---
+++ pomodoro-timer/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import Timer from "./components/Timer/Timer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <Timer />
+    </main>
+  );
+}
+
+export default App;
---
+++ pomodoro-timer/src/components/Timer/Timer.tsx
@@ -0,0 +1,5 @@
+function Timer() {
+  return <div>Timer</div>;
+}
+
+export default Timer;
