181b0bfa18cf2000c33997ef2730d820b828ba4e
Kuba Pawlak <kubapawlak1051@wp.pl>
2023-03-10 23:53:32 +0100

    Modified timerStore, delete content from homepage and globals.css

--- pomodoro-timer/src/pages/_app.tsx
+++ pomodoro-timer/src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Fragment } from "react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>Pomodoro Timer</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Project created with the purpose of learning the fundamentals of Zustand."
+        />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='80'>🍅</text></svg>"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
--- pomodoro-timer/src/store/timerStore.ts
+++ pomodoro-timer/src/store/timerStore.ts
@@ -1,36 +1,49 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
-import { Timer } from "@/types";
+type TimerMode = "work" | "break";
 
-const initialState: Timer = {
-  mode: "pomodoro",
+interface TimerState {
+  mode: TimerMode;
+  workLength: number;
+  breakLength: number;
+  timeLeft: number;
+  isRunning: boolean;
+  pauseTimer: () => void;
+  restartTimer: () => void;
+  changeMode: (mode: TimerMode) => void;
+  changeLength: (sessionType: TimerMode, length: number) => void;
+}
+
+const initialState: TimerState = {
+  mode: "work",
+  workLength: 25,
+  breakLength: 5,
+  timeLeft: 25 * 60,
   isRunning: false,
-  timeRemaining: {
-    minutes: 25,
-    seconds: 0,
-  },
-  settingsVisible: false,
-  settings: {
-    pomodoro: 25,
-    shortBreak: 5,
-    longBreak: 15,
-    longBreakInterval: 5,
-    endSoundVolume: 100,
-    autoContinue: false,
-  },
-  settingsChanged: {
-    pomodoro: false,
-    shortBreak: false,
-    longBreak: false,
-  },
+  pauseTimer: () => {},
+  restartTimer: () => {},
+  changeMode: () => {},
+  changeLength: () => {},
 };
 
-export const useTimerStore = create(
-  persist<Timer>(
-    (set, get) => ({
-      ...initialState,
-    }),
-    { name: "timer" }
-  )
-);
+export const useTimerStore = create<TimerState>((set, get) => ({
+  ...initialState,
+  pauseTimer: () => {
+    set({ isRunning: false });
+  },
+  restartTimer: () => {
+    const mode = get().mode;
+    const timeLeft = get()[`${mode}Length`] * 60;
+    set({ timeLeft, isRunning: true });
+  },
+  changeMode: (mode: TimerMode) => {
+    set({ mode });
+    get().restartTimer();
+  },
+  changeLength: (sessionType: TimerMode, length: number) => {
+    set((_state) => ({ [`${sessionType}Length`]: length }));
+    if (sessionType === get().mode) {
+      set({ timeLeft: length * 60 });
+    }
+  },
+}));
